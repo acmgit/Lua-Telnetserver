a79a01187e45442f233afee05e0395daaf91cd20
A.C.M <undertakers_help@yahoo.com>
2022-01-25 17:55:33 +0100

    Servertuning added. Code commented.

--- Server.lua
+++ Server.lua
@@ -1,21 +1,33 @@
+--[[
+************************************************************************************
+**                                                                                **
+**                            LUA-Telnetserver                                    **
+**                                                                                **
+**                              (?) by A.C.M.                                     **
+**                                 GPL 3.0                                        **
+**                                                                                **
+************************************************************************************
+]]--
+
 local host = "localhost"
-local port = 22
+local port = 2000
+local servertimeout = 0.3
+local clienttimeout = 0.2
+local networktimeout = 0.5
 
 local client= {}
 
 local socket= require("socket")
 local server= assert(socket.bind(host, port))
-local timeout= 0.5
 local crlf = "\r\n"
 local myip, myport = server:getsockname()
-local cidx, clients = 0, 0      -- How many clients?
+local cidx, clients = 0, 0                                  -- Clientnumber, How many clients?
 
-server:settimeout(timeout)
+server:settimeout(servertimeout)
 print("Servername: " .. myip .. ":" .. myport)
 
+-- Sends the given line to all connected Clients, except yourself
 function send_2_clients(ownclient, line)
-    --print("Anzahl Clients: " .. cidx)
-    --print("Sending: " .. line)
     for key, value in pairs(client) do
         if ((client[key] ~= ownclient) and (client[key] ~= nil)) then
             client[key]:send(line .. crlf)
@@ -26,7 +38,7 @@ function send_2_clients(ownclient, line)
 
 end -- function send_2_clients
 
-print("Server is running ...")
+print("Server is running ...")                              -- Only for info
 local newclient, err, line, error
 local receive= {}
 local send={}
@@ -36,38 +48,38 @@ while true do
     -- is a new client connecting?
     newclient, err = server:accept()
     if((err ~= nil) and (err ~= "timeout")) then
-        print(err)                                  -- Something was going wrong
+        print(err)                                          -- Something was going wrong
 
-    elseif (newclient ~= nil) then                  -- new Client connected
+    elseif (newclient ~= nil) then                          -- new Client connected
         cidx = cidx + 1
-        if(cidx >= 10000) then
-            cidx = 1 
-
-        end
-        clients = clients + 1
-        newclient:settimeout(timeout)
+        if(cidx >= 1000) then                               -- Make a reset after 1000 Clients,
+            cidx = 1                                        -- to reuse the Clientnumber again.
+                                                            -- because it's a Clientindex.
+        end -- if(cidx >=
+        clients = clients + 1                               -- Cool, we have a new client
+        newclient:settimeout(clienttimeout)
         newclient:setoption("keepalive", true)
-        client[cidx] = newclient                    -- Store the client
+        client[cidx] = newclient                            -- Store the client
         send_2_clients(client[cidx], "Client " .. cidx .. " is connecting!") -- Send the msg 2 all
-        client[cidx]:send("Hello " .. cidx .. ". Welcome to the Lua-Chat!" .. crlf)
-        print("Clients: " .. clients)
         
-    end
+        client[cidx]:send("Hello " .. cidx .. ". Welcome to the Lua-Chat!" .. crlf)
+        print("Clients: " .. clients)                       -- Only for Info        
+        
+    end -- if((err ~=
 
-    receive, send, error = socket.select(client, nil, timeout)
-    if (receive ~= nil) then                        -- ok, we have messages
+    receive, send, error = socket.select(client, nil, clienttimeout)
+    if (receive ~= nil) then                                -- ok, we have messages
         for key, value in pairs(client) do
             if (value ~= nil) then
                 line, err = value:receive("*l")
                 if ((err ~= nil) and (err ~= "timeout")) then
                     value:shutdown("both")
-                    client[key] = nil                     -- seems connection is closed, so remove it
+                    client[key] = nil                       -- seems connection is closed, so remove it
                     clients = clients - 1
                     send_2_clients(server, "Server: Client " .. key .. " " .. err)
-                    print("Clients: " .. clients)
+                    print("Clients: " .. clients)           -- Only for Info
                     
                 elseif (line ~= nil) then
-                    --print(line)
                     send_2_clients(value, key .. ": " .. line)
 
                 end -- if((err
@@ -77,8 +89,8 @@ while true do
         end -- for i,clients
 
     end -- if(receive
-    socket.sleep(timeout)
+    socket.sleep(networktimeout)
 
 end -- while true
 
-server:close()
+server:close()                                              -- I know, line is unreachable ...
